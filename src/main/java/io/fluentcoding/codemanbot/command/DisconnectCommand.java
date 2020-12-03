c6e703c3b4c3da9b0819b3fdcc538cffbdcf5ec7
FluentCoding <zSuckyLP@gmail.com>
2020-12-03 20:10:46 +0100

    + Abstracted activity updating logic + Update activity when connecting/disconnecting account

--- src/main/java/io/fluentcoding/codemanbot/command/DisconnectCommand.java
+++ src/main/java/io/fluentcoding/codemanbot/command/DisconnectCommand.java
@@ -1,6 +1,7 @@
 package io.fluentcoding.codemanbot.command;
 
 import io.fluentcoding.codemanbot.bridge.DatabaseBridge;
+import io.fluentcoding.codemanbot.util.ActivityUpdater;
 import io.fluentcoding.codemanbot.util.CodeManCommand;
 import io.fluentcoding.codemanbot.util.GlobalVar;
 import net.dv8tion.jda.api.EmbedBuilder;
@@ -15,6 +16,7 @@ public class DisconnectCommand extends CodeManCommand {
     @Override
     public void handle(MessageReceivedEvent e) {
         DatabaseBridge.removeData(e.getAuthor().getIdLong());
+        ActivityUpdater.update(e.getJDA());
 
         EmbedBuilder builder = new EmbedBuilder();
         builder.setColor(GlobalVar.SUCCESS);
---
+++ src/main/java/io/fluentcoding/codemanbot/util/ActivityUpdater.java
@@ -0,0 +1,22 @@
+package io.fluentcoding.codemanbot.util;
+
+import io.fluentcoding.codemanbot.Application;
+import io.fluentcoding.codemanbot.bridge.DatabaseBridge;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+
+public class ActivityUpdater {
+
+    public static void update(JDABuilder builder) {
+        builder.setActivity(getActivityStatus(DatabaseBridge.countDatabase()));
+    }
+
+    public static void update(JDA jda) {
+        jda.getPresence().setActivity(getActivityStatus(DatabaseBridge.countDatabase()));
+    }
+
+    private static Activity getActivityStatus(long count) {
+        return Activity.playing(Application.EXEC_MODE.getCommandPrefix() + "help | " + count + " users");
+    }
+}
