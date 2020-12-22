666afe0229211e6112b0f6477cd1d29e68147499
ananas-dev <lucienfiorini@protonmail.com>
2020-12-22 16:55:56 +0100

    Better username detection

--- src/main/java/io/fluentcoding/codemanbot/util/PatternChecker.java
+++ src/main/java/io/fluentcoding/codemanbot/util/PatternChecker.java
@@ -3,14 +3,13 @@ package io.fluentcoding.codemanbot.util;
 import java.util.regex.Pattern;
 
 public class PatternChecker {
-    private static Pattern connectCodePattern  = Pattern.compile("^([A-Za-z])+#[0-9]{1,3}$"),
-                            slippiUsernamePattern = Pattern.compile("^[A-Za-z0-9$&+,:;=?@#|/'<>.^*()%!_ -]{1,15}$");
+    private static Pattern connectCodePattern = Pattern.compile("^([A-Za-z])+#[0-9]{1,3}$");
 
     public static boolean isConnectCode(String input) {
         return input.length() <= 8 && is(input, connectCodePattern);
     }
     public static boolean isSlippiUsername(String input) {
-        return is(input, slippiUsernamePattern);
+        return input.length() <= 15;
     }
 
     private static boolean is(String input, Pattern pattern) {
