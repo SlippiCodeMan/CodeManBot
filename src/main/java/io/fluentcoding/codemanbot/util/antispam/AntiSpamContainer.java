40ae6ab53a284b381239af58733890784a2bb1d8
FluentCoding <zSuckyLP@gmail.com>
2020-12-24 21:36:12 +0100

    Debug no. 2

--- src/main/java/io/fluentcoding/codemanbot/util/antispam/AntiSpamContainer.java
+++ src/main/java/io/fluentcoding/codemanbot/util/antispam/AntiSpamContainer.java
@@ -3,16 +3,17 @@ package io.fluentcoding.codemanbot.util.antispam;
 import io.github.bucket4j.Bandwidth;
 import io.github.bucket4j.Bucket;
 import io.github.bucket4j.Bucket4j;
+import io.github.bucket4j.Refill;
 
 import java.time.Duration;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 public enum AntiSpamContainer {
     INSTANCE;
 
-    private Bandwidth limit = Bandwidth.simple(5, Duration.ofSeconds(10));
-    private Map<Long, Bucket> userBuckets = new ConcurrentHashMap<>();
+    private Bandwidth limit = Bandwidth.classic(3, Refill.intervally(5, Duration.ofSeconds(5)));
+    private Map<Long, Bucket> userBuckets = new HashMap<>();
 
     public boolean userAllowedToAction(Long userId) {
         Bucket bucket = userBuckets.get(userId);
