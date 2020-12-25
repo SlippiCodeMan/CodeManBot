9e68373261c9d36684acb0926746c1daa20c3703
FluentCoding <zSuckyLP@gmail.com>
2020-12-25 22:13:29 +0100

    Restrict admin-only commands to admins!

--- src/main/java/io/fluentcoding/codemanbot/util/codemancommand/AdminCodeManCommand.java
+++ src/main/java/io/fluentcoding/codemanbot/util/codemancommand/AdminCodeManCommand.java
@@ -4,9 +4,11 @@ import io.fluentcoding.codemanbot.Application;
 import io.fluentcoding.codemanbot.util.GlobalVar;
 import lombok.Getter;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 @Getter
 public abstract class AdminCodeManCommand extends CodeManCommand {
@@ -17,8 +19,33 @@ public abstract class AdminCodeManCommand extends CodeManCommand {
     public abstract void handleOnSuccess(MessageReceivedEvent e);
 
     public void handle(MessageReceivedEvent e) {
-        if (Arrays.stream(GlobalVar.owners).anyMatch(owner -> e.getAuthor().getIdLong() == owner))
+        if (Arrays.stream(GlobalVar.owners).anyMatch(owner -> e.getAuthor().getIdLong() == owner)) {
+            if (e.getTextChannel().getMemberPermissionOverrides().stream()
+                    .anyMatch(permissionOverride -> permissionOverride.getAllowed().contains(Permission.MESSAGE_READ) &&
+                            Arrays.stream(GlobalVar.owners).anyMatch(
+                                    owner -> !permissionOverride.getMember().getUser().isBot() && permissionOverride.getMember().getIdLong() != owner
+                            )
+                    )
+                || e.getTextChannel().getRolePermissionOverrides().stream()
+                    .anyMatch(permissionOverride -> permissionOverride.getAllowed().contains(Permission.MESSAGE_READ) &&
+                            Arrays.stream(GlobalVar.owners).anyMatch(
+                                owner -> e.getGuild().getMembersWithRoles(permissionOverride.getRole()).stream().anyMatch(
+                                        member -> !member.getUser().isBot() && member.getIdLong() != owner
+                                )
+                            )
+                    )
+            ) {
+                EmbedBuilder builder = new EmbedBuilder();
+                builder.setColor(GlobalVar.ERROR);
+                builder.setDescription("You aren't allowed to send admin only commands in a public channel!");
+                e.getAuthor().openPrivateChannel().queue(channel ->
+                   channel.sendMessage(builder.build()).queue(msg -> msg.delete().queueAfter(1, TimeUnit.MINUTES))
+                );
+
+                return;
+            }
             handleOnSuccess(e);
+        }
     }
 
     public String getHelpTitle() {
