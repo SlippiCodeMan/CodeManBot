7fc5cef1cbfb17effefd93eb67d11cc0ba91c73d
FluentCoding <zSuckyLP@gmail.com>
2020-12-25 23:44:40 +0100

    Bugs fixed?

--- src/main/java/io/fluentcoding/codemanbot/util/codemancommand/AdminCodeManCommand.java
+++ src/main/java/io/fluentcoding/codemanbot/util/codemancommand/AdminCodeManCommand.java
@@ -6,6 +6,7 @@ import io.fluentcoding.codemanbot.util.SystemUtil;
 import lombok.Getter;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.IPermissionHolder;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 import java.util.Arrays;
@@ -31,17 +32,13 @@ public abstract class AdminCodeManCommand extends CodeManCommand {
                             )
                     )
                 || e.getTextChannel().getRolePermissionOverrides().stream()
-                    .anyMatch(permissionOverride -> {
-                        System.out.println(permissionOverride.getRole().getName());
-                        return permissionOverride.getAllowed().contains(Permission.MESSAGE_READ) && // WHEN MESSAGES READABLE
-                                ( // WHEN ITS NOT A BOT AND NOT AN OWNER
-                                        e.getGuild().getMembersWithRoles(permissionOverride.getRole()).stream().anyMatch(member -> {
-                                            System.out.println(member.getUser().getAsTag() + " " + !Arrays.stream(GlobalVar.owners).anyMatch(owner -> member.getIdLong() == owner));
-                                            return !member.getUser().isBot() &&
-                                                    !Arrays.stream(GlobalVar.owners).anyMatch(owner -> member.getIdLong() == owner);
-                                        })
-                                );
-                    })
+                    .anyMatch(permissionOverride ->
+                            permissionOverride.getRole().hasPermission(e.getTextChannel(), Permission.MESSAGE_READ) &&
+                        e.getGuild().getMembersWithRoles(permissionOverride.getRole()).stream().anyMatch(member ->
+                            !member.getUser().isBot() &&
+                            !Arrays.stream(GlobalVar.owners).anyMatch(owner -> member.getIdLong() == owner)
+                        )
+                    )
             ) {
                 e.getMessage().delete().queue();
 
