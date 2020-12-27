880950a6c71c2e26bff101b09fbbb272d008ea23
FluentCoding <zSuckyLP@gmail.com>
2020-12-27 16:05:38 +0100

    Fix AskCommand when being disconnected

--- src/main/java/io/fluentcoding/codemanbot/command/AskCommand.java
+++ src/main/java/io/fluentcoding/codemanbot/command/AskCommand.java
@@ -1,5 +1,6 @@
 package io.fluentcoding.codemanbot.command;
 
+import io.fluentcoding.codemanbot.Application;
 import io.fluentcoding.codemanbot.bridge.DatabaseBridge;
 import io.fluentcoding.codemanbot.util.codemancommand.CodeManCommand;
 import io.fluentcoding.codemanbot.util.GlobalVar;
@@ -16,13 +17,19 @@ public class AskCommand extends CodeManCommand {
     public void handle(GuildMessageReceivedEvent e) {
         EmbedBuilder builder = new EmbedBuilder();
 
-        builder.setTitle("Netplay Search");
-        builder.setDescription(e.getAuthor().getName() + " is looking for an opponent!");
-        builder.setThumbnail(e.getAuthor().getAvatarUrl());
-        builder.setColor(GlobalVar.SUCCESS);
+        String code = DatabaseBridge.getCode(e.getAuthor().getIdLong());
+        if (code == null) {
+            builder.setDescription("You haven't connected to CodeMan yet! Take a look at **" + Application.EXEC_MODE.getCommandPrefix() + "connect**!");
+            builder.setColor(GlobalVar.ERROR);
+        } else {
+            builder.setTitle("Netplay Search");
+            builder.setDescription(e.getAuthor().getName() + " is looking for an opponent!");
+            builder.setThumbnail(e.getAuthor().getAvatarUrl());
+            builder.setColor(GlobalVar.SUCCESS);
 
-        builder.addField("Their code", DatabaseBridge.getCode(e.getAuthor().getIdLong()), false);
-        builder.setFooter(GlobalVar.FROG_EMOJI + " slippi 2.x.x");
+            builder.addField("Their code", code, false);
+            builder.setFooter(GlobalVar.FROG_EMOJI + " slippi 2.x.x");
+        }
 
         e.getChannel().sendMessage(builder.build()).queue();
     }
