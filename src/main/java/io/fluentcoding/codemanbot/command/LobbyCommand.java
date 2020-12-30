732522c45a7567bae5a895ff517a73631393b128
ananas-dev <lucienfiorini@protonmail.com>
2020-12-30 19:02:30 +0100

    FIx shit

--- src/main/java/io/fluentcoding/codemanbot/command/LobbyCommand.java
+++ src/main/java/io/fluentcoding/codemanbot/command/LobbyCommand.java
@@ -2,59 +2,52 @@ package io.fluentcoding.codemanbot.command;
 
 import io.fluentcoding.codemanbot.bridge.DatabaseBridge;
 import io.fluentcoding.codemanbot.util.StringUtil;
-import io.fluentcoding.codemanbot.util.codemancommand.CodeManCommand;
+import io.fluentcoding.codemanbot.util.codemancommand.CodeManCommandWithArgs;
+import io.fluentcoding.codemanbot.util.CodeManArgumentSet;
 import io.fluentcoding.codemanbot.util.EmbedUtil;
 import io.fluentcoding.codemanbot.util.GlobalVar;
 import io.fluentcoding.codemanbot.util.ssbm.SSBMCharacter;
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
-public class LobbyCommand extends CodeManCommand {
+public class LobbyCommand extends CodeManCommandWithArgs {
 
-    public LobbyCommand(String description, String prefix, String... aliases) {
-        super(description, prefix, aliases);
+    public LobbyCommand(CodeManArgumentSet argSet, String description, String prefix, String... aliases) {
+        super(argSet, description, prefix, aliases);
     }
 
     @Override
-    public void handle(GuildMessageReceivedEvent e) {
-        // Set settings
-        // WIP
-        EmbedBuilder lobby = new EmbedBuilder();
+    public void handle(GuildMessageReceivedEvent e, Map<String, String> args) {
+        EmbedBuilder builder = new EmbedBuilder();
+        String description = args.get("description");
 
         String code = DatabaseBridge.getCode(e.getAuthor().getIdLong());
         if (code == null) {
-            lobby = EmbedUtil.notConnected(lobby);
+            builder = EmbedUtil.notConnected(builder);
         } else {
             e.getMessage().delete().queue();
 
-            EmbedBuilder stepOne = new EmbedBuilder();
-            stepOne.setTitle("Lobby Creator");
-            stepOne.setDescription("Enter the description of your lobby:");
-            e.getChannel().sendMessage(stepOne.build()).queue();
+            builder.setAuthor(e.getAuthor().getName(), null, e.getAuthor().getAvatarUrl());
+            builder.setTitle("Searching `[1/2]`");
 
-            String content = e.getMessage().getContentRaw();
-            if (!content.isEmpty()) {
-                lobby.setDescription(content);
-                e.getJDA().removeEventListener(this); // stop listening
-            }
+            if (description != null)
+                builder.setDescription(description);
 
-            lobby.setAuthor(e.getAuthor().getName(), null, e.getAuthor().getAvatarUrl());
-            lobby.setTitle("Searching `[1/2]`");
-            lobby.setColor(GlobalVar.WAITING);
+            builder.setColor(GlobalVar.WAITING);
 
             List<SSBMCharacter> characters = DatabaseBridge.getMains(e.getAuthor().getIdLong());
             if (characters != null && characters.size() != 0) {
-                lobby.addField("Mains", StringUtil.getMainsFormatted(characters), true);
+                builder.addField("Mains", StringUtil.getMainsFormatted(characters), true);
             }
 
             Random random = new Random();
 
-            lobby.setFooter("ID: " + String.format("%03d", random.nextInt(9999 - 0 + 1) + 0));
+            builder.setFooter("ID: " + String.format("%03d", random.nextInt(1000)));
         }
-        e.getChannel().sendMessage(lobby.build()).queue(msg -> msg.addReaction(GlobalVar.CHECKMARK_EMOJI).queue());
+        e.getChannel().sendMessage(builder.build()).queue(msg -> msg.addReaction(GlobalVar.CHECKMARK_EMOJI).queue());
     }
 }
