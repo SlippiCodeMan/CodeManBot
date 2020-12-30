5ea9499c12b76967bea5c481c68a3a7307e3a625
ananas-dev <lucienfiorini@protonmail.com>
2020-12-30 17:29:21 +0100

    WIP Lobby creation menu

--- src/main/java/io/fluentcoding/codemanbot/command/LobbyCommand.java
+++ src/main/java/io/fluentcoding/codemanbot/command/LobbyCommand.java
@@ -7,6 +7,7 @@ import io.fluentcoding.codemanbot.util.EmbedUtil;
 import io.fluentcoding.codemanbot.util.GlobalVar;
 import io.fluentcoding.codemanbot.util.ssbm.SSBMCharacter;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
 import java.util.List;
@@ -22,28 +23,38 @@ public class LobbyCommand extends CodeManCommand {
     public void handle(GuildMessageReceivedEvent e) {
         // Set settings
         // WIP
-        EmbedBuilder builder = new EmbedBuilder();
+        EmbedBuilder lobby = new EmbedBuilder();
 
         String code = DatabaseBridge.getCode(e.getAuthor().getIdLong());
         if (code == null) {
-            builder = EmbedUtil.notConnected(builder);
+            lobby = EmbedUtil.notConnected(lobby);
         } else {
             e.getMessage().delete().queue();
 
-            builder.setAuthor(e.getAuthor().getName(), null, e.getAuthor().getAvatarUrl());
-            builder.setTitle("Searching `[1/2]`");
-            builder.setDescription(StringUtil.bold("quick sesh"));
-            builder.setColor(GlobalVar.WAITING);
+            EmbedBuilder stepOne = new EmbedBuilder();
+            stepOne.setTitle("Lobby Creator");
+            stepOne.setDescription("Enter the description of your lobby:");
+
+            String content = e.getMessage().getContentRaw();
+            if (!content.isEmpty()) {
+                lobby.setDescription(content);
+                e.getJDA().removeEventListener(this); // stop listening
+            }
+
+            lobby.setAuthor(e.getAuthor().getName(), null, e.getAuthor().getAvatarUrl());
+            lobby.setTitle("Searching `[1/2]`");
+            lobby.setDescription(StringUtil.bold("quick sesh"));
+            lobby.setColor(GlobalVar.WAITING);
 
             List<SSBMCharacter> characters = DatabaseBridge.getMains(e.getAuthor().getIdLong());
             if (characters != null && characters.size() != 0) {
-                builder.addField("Mains", StringUtil.getMainsFormatted(characters), true);
+                lobby.addField("Mains", StringUtil.getMainsFormatted(characters), true);
             }
 
             Random random = new Random();
 
-            builder.setFooter("ID: " + String.format("%03d", random.nextInt(9999 - 0 + 1) + 0));
+            lobby.setFooter("ID: " + String.format("%03d", random.nextInt(9999 - 0 + 1) + 0));
         }
-        e.getChannel().sendMessage(builder.build()).queue(msg -> msg.addReaction(GlobalVar.CHECKMARK_EMOJI).queue());
+        e.getChannel().sendMessage(lobby.build()).queue(msg -> msg.addReaction(GlobalVar.CHECKMARK_EMOJI).queue());
     }
 }
