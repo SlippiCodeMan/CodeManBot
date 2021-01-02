83f040ab934360f1f7f8334b518ab07bee37c140
ananas-dev <lucienfiorini@protonmail.com>
2021-01-02 14:22:41 +0100

    WIP Tournament command

---
+++ src/main/java/io/fluentcoding/codemanbot/command/TournamentInfoCommand.java
@@ -0,0 +1,40 @@
+package io.fluentcoding.codemanbot.command;
+
+import io.fluentcoding.codemanbot.bridge.ChallongeBridge;
+import io.fluentcoding.codemanbot.bridge.DatabaseBridge;
+import io.fluentcoding.codemanbot.bridge.SlippiBridge;
+import io.fluentcoding.codemanbot.bridge.ChallongeBridge.TournamentEntry;
+import io.fluentcoding.codemanbot.util.*;
+import io.fluentcoding.codemanbot.util.codemancommand.CodeManCommand;
+import io.fluentcoding.codemanbot.container.PagingContainer;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+public class TournamentInfoCommand extends CodeManCommand {
+
+    public TournamentInfoCommand(CodeManArgumentSet argSet, String description, String name, String... aliases) {
+        super(argSet, description, name, aliases);
+    }
+
+    @Override
+    public void handle(GuildMessageReceivedEvent e, Map<String, String> args) {
+        String url = args.get("url");
+
+        EmbedBuilder builder = new EmbedBuilder();
+
+        TournamentEntry tournament = ChallongeBridge.getTournament(url);
+        builder.setAuthor("Challonge");
+        builder.setTitle(tournament.getName());
+        builder.setColor(GlobalVar.CHALLONGE);
+    }
+
+}
