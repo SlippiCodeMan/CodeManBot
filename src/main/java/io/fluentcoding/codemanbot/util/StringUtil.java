2d60458c4ebb654078ac4c1d8f328ea8968cfbb5
ananas-dev <lucienfiorini@protonmail.com>
2021-01-02 21:48:32 +0100

    test 3

--- src/main/java/io/fluentcoding/codemanbot/util/StringUtil.java
+++ src/main/java/io/fluentcoding/codemanbot/util/StringUtil.java
@@ -52,7 +52,7 @@ public class StringUtil {
     public static String makeSponsorBold(String input) {
         Pattern stringPattern = Pattern.compile("^[[A-Z1-9]]+");
         Matcher matcher = stringPattern.matcher(input);
-        return input.replaceAll(matcher.group(0), "**" + matcher.group(0) + "**");
+        return input.replaceAll(matcher.toString(), "**" + matcher.toString() + "**");
     }
     public static String bold(String input) {
         return "**" + input + "**";
