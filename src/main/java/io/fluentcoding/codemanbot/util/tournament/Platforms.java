7fc68b254812a8515bb7880ba3db597da77928f3
ananas-dev <lucienfiorini@protonmail.com>
2021-01-03 18:15:22 +0100

    Add tournament providers

---
+++ src/main/java/io/fluentcoding/codemanbot/util/tournament/Platforms.java
@@ -0,0 +1,14 @@
+package io.fluentcoding.codemanbot.util.tournament;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum Platforms {
+    CHALLONGE("challonge", "https://challonge.com/"),
+    SMASHGG("smash.gg", "https://smash.gg");
+
+    String name;
+    String url;
+}
