faed702b7c9d30285e24d9307196161df3e2d757
ananas-dev <lucienfiorini@protonmail.com>
2021-01-03 15:06:25 +0100

    Remove the awaiting_review condition

--- src/main/java/io/fluentcoding/codemanbot/command/TournamentInfoCommand.java
+++ src/main/java/io/fluentcoding/codemanbot/command/TournamentInfoCommand.java
@@ -97,25 +97,6 @@ public class TournamentInfoCommand extends CodeManCommand {
                                                             seperateCodeFromUsername.get("code"))));
                                 })
                                 .collect(Collectors.joining("\n")), true);
-                    } else if (tournament.getState().equals("awaiting_review")) {
-                        newBuilder.addField("Current Results", participants.stream()
-                                .filter(participant -> participant.getFinalRank() <= 5 && participant.getFinalRank() != 0)
-                                .sorted((o1,o2) -> Integer.compare(o1.getFinalRank(), o2.getFinalRank()))
-                                .map(participant -> {
-                                    Map<String, String> seperateCodeFromUsername = StringUtil.separateCodeFromUsername(
-                                            participant.getDisplayName()
-                                    );
-
-                                    return StringUtil.bold(participant.getFinalRank()
-                                            + ". ")
-                                            + StringUtil.removeHardcodedSeeding(seperateCodeFromUsername.get("username"))
-                                            + " "
-                                            + StringUtil.getMainsFormatted(
-                                            DatabaseBridge.getMains(
-                                                    DatabaseBridge.getDiscordIdFromConnectCode(
-                                                            seperateCodeFromUsername.get("code"))));
-                                })
-                                .collect(Collectors.joining("\n")), true);
                     } else {
                         newBuilder.addField("Seeding", participants.stream()
                                 .filter(participant -> participant.getSeed() <= 9)
