044217ab6b5fe2e515c27e89b3d096ba93e171ec
ananas-dev <lucienfiorini@protonmail.com>
2021-01-04 12:56:37 +0100

    Add tournament platform detection

--- src/main/java/io/fluentcoding/codemanbot/command/TournamentInfoCommand.java
+++ src/main/java/io/fluentcoding/codemanbot/command/TournamentInfoCommand.java
@@ -6,6 +6,7 @@ import io.fluentcoding.codemanbot.bridge.ChallongeBridge.ParticipantEntry;
 import io.fluentcoding.codemanbot.bridge.ChallongeBridge.TournamentEntry;
 import io.fluentcoding.codemanbot.util.*;
 import io.fluentcoding.codemanbot.util.codemancommand.CodeManCommand;
+import io.fluentcoding.codemanbot.util.tournament.Platforms;
 import io.fluentcoding.codemanbot.util.tournament.RankEmotes;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
@@ -42,97 +43,108 @@ public class TournamentInfoCommand extends CodeManCommand {
             finalUrl = url;
         }
 
+        Platforms platform = Arrays.stream(Platforms.values())
+                .filter(item -> url.contains(item.getUrl()))
+                .findFirst().orElse(null);
+
+
         EmbedBuilder builder = new EmbedBuilder();
 
         builder.setTitle(GlobalVar.LOADING_EMOJI);
         builder.setColor(GlobalVar.LOADING);
-        Future<TournamentEntry> tournamentFuture = Executors.newCachedThreadPool().submit(() -> ChallongeBridge.getTournament(finalUrl));
-        Future<List<ParticipantEntry>> participantFuture = Executors.newCachedThreadPool().submit(() -> ChallongeBridge.getParticipants(finalUrl));
-        e.getChannel().sendMessage(builder.build()).queue(msg -> {
-            TournamentEntry tournament;
-            List<ParticipantEntry> participants;
-            try {
-                tournament = tournamentFuture.get();
-                participants = participantFuture.get();
-            } catch (Exception ex) {
-                ex.printStackTrace();
-                tournament = null;
-                participants = null;
-            }
-
-            EmbedBuilder newBuilder = new EmbedBuilder();
-
-            if (tournament != null) {
-                newBuilder.setAuthor("Challonge", "https://challonge.com", "https://codeman.rocks/assets/challonge.png");
-                newBuilder.setTitle(tournament.getName(), isUrl ? StringUtil.makeUrlValid(url) : "https://challonge.com/" + url);
-
-                String description = StringUtil.getTextFromHtml(tournament.getDescription());
-                if (!description.isEmpty()) {
-                    if (description.length() > 300)
-                        description = description.substring(0, 301) + "...";
-
-                    newBuilder.setDescription(description);
+        if (platform == Platforms.CHALLONGE) {
+            Future<TournamentEntry> tournamentFuture = Executors.newCachedThreadPool().submit(() -> ChallongeBridge.getTournament(finalUrl));
+            Future<List<ParticipantEntry>> participantFuture = Executors.newCachedThreadPool().submit(() -> ChallongeBridge.getParticipants(finalUrl));
+            e.getChannel().sendMessage(builder.build()).queue(msg -> {
+                TournamentEntry tournament;
+                List<ParticipantEntry> participants;
+                try {
+                    tournament = tournamentFuture.get();
+                    participants = participantFuture.get();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                    tournament = null;
+                    participants = null;
                 }
 
-                if (participants != null) {
-                    if (tournament.getState().equals("complete")) {
-                        newBuilder.addField("Final Results", participants.stream()
-                                .filter(participant -> participant.getFinalRank() <= 5 && participant.getFinalRank() != 0)
-                                .sorted((o1,o2) -> Integer.compare(o1.getFinalRank(), o2.getFinalRank()))
-                                .map(participant -> {
-                                    Map<String, String> seperateCodeFromUsername = StringUtil.separateCodeFromUsername(
-                                            participant.getDisplayName()
-                                    );
-                                    RankEmotes rankEmote = Arrays.stream(RankEmotes.values())
-                                            .filter(emote -> participant.getFinalRank() == emote.getNumber())
-                                            .findFirst().orElse(null);
-                                    String prefix = rankEmote == null ? StringUtil.bold(participant.getFinalRank() + "th") : rankEmote.getEmote();
-                                    return prefix
-                                            + " "
-                                            + StringUtil.removeHardcodedSeeding(seperateCodeFromUsername.get("username"))
-                                            + " "
-                                            + StringUtil.getMainsFormatted(
-                                            DatabaseBridge.getMains(
-                                                    DatabaseBridge.getDiscordIdFromConnectCode(
-                                                            seperateCodeFromUsername.get("code"))));
-                                })
-                                .collect(Collectors.joining("\n")), true);
-                    } else {
-                        newBuilder.addField("Seeding", participants.stream()
-                                .filter(participant -> participant.getSeed() <= 9)
-                                .sorted((o1,o2) -> Integer.compare(o1.getSeed(), o2.getSeed()))
-                                .map(participant -> {
-                                    Map<String, String> seperateCodeFromUsername = StringUtil.separateCodeFromUsername(
-                                            participant.getDisplayName()
-                                    );
+                EmbedBuilder newBuilder = new EmbedBuilder();
 
-                                    return StringUtil.bold(participant.getSeed()
-                                            + ". ")
-                                            + StringUtil.removeHardcodedSeeding(seperateCodeFromUsername.get("username"))
-                                            + " "
-                                            + StringUtil.getMainsFormatted(
-                                            DatabaseBridge.getMains(
-                                                    DatabaseBridge.getDiscordIdFromConnectCode(
-                                                            seperateCodeFromUsername.get("code"))));
-                                })
-                                .collect(Collectors.joining("\n")), true);
+                if (tournament != null) {
+                    newBuilder.setAuthor("Challonge", "https://challonge.com", "https://codeman.rocks/assets/challonge.png");
+                    newBuilder.setTitle(tournament.getName(), isUrl ? StringUtil.makeUrlValid(url) : "https://challonge.com/" + url);
+
+                    String description = StringUtil.getTextFromHtml(tournament.getDescription());
+                    if (!description.isEmpty()) {
+                        if (description.length() > 300)
+                            description = description.substring(0, 301) + "...";
+
+                        newBuilder.setDescription(description);
                     }
+
+                    if (participants != null) {
+                        if (tournament.getState().equals("complete")) {
+                            newBuilder.addField("Final Results", participants.stream()
+                                    .filter(participant -> participant.getFinalRank() <= 5 && participant.getFinalRank() != 0)
+                                    .sorted((o1,o2) -> Integer.compare(o1.getFinalRank(), o2.getFinalRank()))
+                                    .map(participant -> {
+                                        Map<String, String> seperateCodeFromUsername = StringUtil.separateCodeFromUsername(
+                                                participant.getDisplayName()
+                                        );
+                                        RankEmotes rankEmote = Arrays.stream(RankEmotes.values())
+                                                .filter(emote -> participant.getFinalRank() == emote.getNumber())
+                                                .findFirst().orElse(null);
+                                        String prefix = rankEmote == null ? StringUtil.bold(participant.getFinalRank() + "th") : rankEmote.getEmote();
+                                        return prefix
+                                                + " "
+                                                + StringUtil.removeHardcodedSeeding(seperateCodeFromUsername.get("username"))
+                                                + " "
+                                                + StringUtil.getMainsFormatted(
+                                                DatabaseBridge.getMains(
+                                                        DatabaseBridge.getDiscordIdFromConnectCode(
+                                                                seperateCodeFromUsername.get("code"))));
+                                    })
+                                    .collect(Collectors.joining("\n")), true);
+                        } else {
+                            newBuilder.addField("Seeding", participants.stream()
+                                    .filter(participant -> participant.getSeed() <= 9)
+                                    .sorted((o1,o2) -> Integer.compare(o1.getSeed(), o2.getSeed()))
+                                    .map(participant -> {
+                                        Map<String, String> seperateCodeFromUsername = StringUtil.separateCodeFromUsername(
+                                                participant.getDisplayName()
+                                        );
+
+                                        return StringUtil.bold(participant.getSeed()
+                                                + ". ")
+                                                + StringUtil.removeHardcodedSeeding(seperateCodeFromUsername.get("username"))
+                                                + " "
+                                                + StringUtil.getMainsFormatted(
+                                                DatabaseBridge.getMains(
+                                                        DatabaseBridge.getDiscordIdFromConnectCode(
+                                                                seperateCodeFromUsername.get("code"))));
+                                    })
+                                    .collect(Collectors.joining("\n")), true);
+                        }
+                    }
+
+                    newBuilder.addField("Infos", 
+                            StringUtil.bold("- Game: ") + tournament.getGameName() + "\n"
+                            + StringUtil.bold("- Type: ") + tournament.getType() + "\n"
+                            + StringUtil.bold("- Attendees: ") + tournament.getParticipantsCount() + "\n"
+                            + StringUtil.bold("- State: ") + StringUtil.removeUnderscores(tournament.getState())
+                            , true);
+
+                    newBuilder.setFooter(StringUtil.formatIsoDateAndTime(tournament.getStartsAt()));
+                    newBuilder.setColor(GlobalVar.CHALLONGE);
+                } else {
+                    newBuilder.setDescription("Operation failed: Tournament not found!");
+                    newBuilder.setColor(GlobalVar.ERROR);
                 }
-
-                newBuilder.addField("Infos", 
-                        StringUtil.bold("- Game: ") + tournament.getGameName() + "\n"
-                        + StringUtil.bold("- Type: ") + tournament.getType() + "\n"
-                        + StringUtil.bold("- Attendees: ") + tournament.getParticipantsCount() + "\n"
-                        + StringUtil.bold("- State: ") + StringUtil.removeUnderscores(tournament.getState())
-                        , true);
-
-                newBuilder.setFooter(StringUtil.formatIsoDateAndTime(tournament.getStartsAt()));
-                newBuilder.setColor(GlobalVar.CHALLONGE);
-            } else {
-                newBuilder.setDescription("Operation failed: Tournament not found!");
-                newBuilder.setColor(GlobalVar.ERROR);
-            }
-            msg.editMessage(newBuilder.build()).queue();
-        });
+                msg.editMessage(newBuilder.build()).queue();
+            });
+        } else if (platform == Platforms.SMASHGG) {
+            builder.setDescription("Operation failed:" + StringUtil.oneLineCodeBlock("Smash.gg") + "is not supported yet!");
+            builder.setColor(GlobalVar.ERROR);
+            e.getChannel().sendMessage(builder.build()).queue();
+        }
     }
 }
