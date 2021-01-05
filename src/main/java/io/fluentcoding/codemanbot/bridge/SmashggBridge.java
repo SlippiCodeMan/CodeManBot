35f1fe39b86e97d5ae09077bd974f0a654598d61
ananas-dev <lucienfiorini@protonmail.com>
2021-01-05 10:41:09 +0100

    wops

--- src/main/java/io/fluentcoding/codemanbot/bridge/SmashggBridge.java
+++ src/main/java/io/fluentcoding/codemanbot/bridge/SmashggBridge.java
@@ -43,7 +43,7 @@ public class SmashggBridge {
 
             OwnerEntry owner = new OwnerEntry(
                 ownerObject.getString("name"),
-                ownerObject.getJSONArray("image").isNull(0) ? "" : ownerObject.getJSONArray("images").getJSONObject(0).getString("url"));
+                ownerObject.getJSONArray("images").isNull(0) ? "" : ownerObject.getJSONArray("images").getJSONObject(0).getString("url"));
 
             if (eventArray.length() == 0)
                 return null;
