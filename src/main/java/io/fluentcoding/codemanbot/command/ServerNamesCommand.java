26dd622b97620da528b975a4e25b5333bdde24e9
FluentCoding <zSuckyLP@gmail.com>
2021-08-29 17:20:15 +0200

    &ServerNames

---
+++ src/main/java/io/fluentcoding/codemanbot/command/ServerNamesCommand.java
@@ -0,0 +1,41 @@
+package io.fluentcoding.codemanbot.command;
+
+import io.fluentcoding.codemanbot.bridge.ChallongeBridge;
+import io.fluentcoding.codemanbot.bridge.DatabaseBridge;
+import io.fluentcoding.codemanbot.bridge.SlippiBotBridge;
+import io.fluentcoding.codemanbot.bridge.SlippiBridge;
+import io.fluentcoding.codemanbot.container.PagingContainer;
+import io.fluentcoding.codemanbot.util.GlobalVar;
+import io.fluentcoding.codemanbot.util.SystemUtil;
+import io.fluentcoding.codemanbot.util.codemancommand.DevCodeManCommand;
+import io.fluentcoding.codemanbot.util.StringUtil;
+import io.fluentcoding.codemanbot.util.hook.ListenerHook;
+import lombok.Getter;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+public class ServerNamesCommand extends DevCodeManCommand {
+
+    public ServerNamesCommand(String name, String... aliases) {
+        super(name, aliases);
+    }
+
+    @Override
+    public void handleOnSuccess(GuildMessageReceivedEvent e, Map<String, String> args) {
+        PagingContainer.INSTANCE.pageableMessageHandler(e.getChannel()::sendMessage,
+                new PagingContainer.PageableContent(StringUtil.bold( e.getJDA().getGuilds().size() + " servers are using this bot:") + "\n\n",
+                        e.getJDA().getGuilds().stream().map(
+                                guild -> guild.getName() + " (Region: " + (guild.getVoiceChannels().size() == 0 ? "Unknown" : guild.getVoiceChannels().get(0).getRegionRaw()) + ")"
+                        ).toArray(String[]::new), e.getAuthor().getIdLong()));
+    }
+}
