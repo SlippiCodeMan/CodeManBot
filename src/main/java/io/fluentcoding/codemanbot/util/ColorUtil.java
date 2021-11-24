1c5cd34c0697136c67b64201c6f180ca4c293fae
FluentCoding <zSuckyLP@gmail.com>
2021-11-24 20:01:38 +0100

    loading should work now

--- src/main/java/io/fluentcoding/codemanbot/util/ColorUtil.java
+++ src/main/java/io/fluentcoding/codemanbot/util/ColorUtil.java
@@ -3,22 +3,22 @@ package io.fluentcoding.codemanbot.util;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ColorUtil {
     private static Map<String, Integer> colorMap = new HashMap<>();
 
-    public static void init() throws URISyntaxException, IOException, JSONException {
-        URL resource = Thread.currentThread().getContextClassLoader().getResource("colors.json");
-        byte[] bytes = Files.readAllBytes(Paths.get(resource.toURI()));
-        String jsonText = new String(bytes);
+    public static void init() throws JSONException {
+        String jsonText = new BufferedReader(
+                new InputStreamReader(Thread.currentThread().getContextClassLoader().getResourceAsStream("colors.json"), StandardCharsets.UTF_8)
+        ).lines().collect(Collectors.joining("\n"));
 
         JSONObject jsonObject = new JSONObject(jsonText);
         Iterator<String> keys = jsonObject.keys();
