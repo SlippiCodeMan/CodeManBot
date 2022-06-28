6c61d1eaed1c4b233a72d2d6e6a3ffec832fcc13
lucien <lucienfiorini@protonmail.com>
2022-06-28 21:06:08 +0200

    remove useralreadyconnected check

--- src/main/java/io/fluentcoding/codemanbot/command/ConnectCommand.java
+++ src/main/java/io/fluentcoding/codemanbot/command/ConnectCommand.java
@@ -8,6 +8,7 @@ import io.fluentcoding.codemanbot.util.*;
 import io.fluentcoding.codemanbot.util.codemancommand.CodeManCommand;
 import io.fluentcoding.codemanbot.util.GlobalVar;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import org.json.JSONException;
 
@@ -64,35 +65,27 @@ public class ConnectCommand extends CodeManCommand {
                     newBuilder.setDescription("This connect code doesn't exist!");
                     ConnectContainer.INSTANCE.removeConnectInformation(information);
                 } else {
-                    boolean codeAlreadyTaken = DatabaseBridge.codeAlreadyTaken(code);
+                    if (SlippiBotBridge.isConnected()) {
+                        newBuilder.setDescription("We've sent you the instructions via DM on how to connect your account!");
+                        newBuilder.setColor(GlobalVar.SUCCESS);
 
-                    if (!codeAlreadyTaken) {
-                        if (SlippiBotBridge.isConnected()) {
-                            newBuilder.setDescription("We've sent you the instructions via DM on how to connect your account!");
-                            newBuilder.setColor(GlobalVar.SUCCESS);
-
-                            e.getAuthor().openPrivateChannel().queue(privateChannel -> {
-                                ConnectContainer.INSTANCE.setPrivateChannel(information, privateChannel);
-                                try {
-                                    SlippiBotBridge.sendQueue(information);
-                                } catch (JSONException | IOException jsonException) {
-                                    jsonException.printStackTrace();
-                                    ConnectContainer.INSTANCE.removeConnectInformation(information);
-                                }
-                            });
-                        } else if (GlobalVar.dotenv.get("CODEMAN_EXEC_MODE").equals("dev")) {
-                            ConnectContainer.INSTANCE.removeConnectInformation(information);
-                            DatabaseBridge.insertCode(information.getUserId(), information.getCode());
-                            newBuilder.setDescription("Success!");
-                            newBuilder.setColor(GlobalVar.SUCCESS);
-                        } else {
-                            newBuilder.setDescription("We weren't able to connect to our bot service!");
-                            newBuilder.setColor(GlobalVar.ERROR);
-                            ConnectContainer.INSTANCE.removeConnectInformation(information);
-                        }
+                        e.getAuthor().openPrivateChannel().queue(privateChannel -> {
+                            ConnectContainer.INSTANCE.setPrivateChannel(information, privateChannel);
+                            try {
+                                SlippiBotBridge.sendQueue(information);
+                            } catch (JSONException | IOException jsonException) {
+                                jsonException.printStackTrace();
+                                ConnectContainer.INSTANCE.removeConnectInformation(information);
+                            }
+                        });
+                    } else if (GlobalVar.dotenv.get("CODEMAN_EXEC_MODE").equals("dev")) {
+                        ConnectContainer.INSTANCE.removeConnectInformation(information);
+                        DatabaseBridge.insertCode(information.getUserId(), information.getCode());
+                        newBuilder.setDescription("Success!");
+                        newBuilder.setColor(GlobalVar.SUCCESS);
                     } else {
+                        newBuilder.setDescription("We weren't able to connect to our bot service!");
                         newBuilder.setColor(GlobalVar.ERROR);
-                        newBuilder.setDescription("Operation failed! Someone already uses this code!\nContact **Ananas#5903** or **FluentCoding#3314**!");
                         ConnectContainer.INSTANCE.removeConnectInformation(information);
                     }
                 }
